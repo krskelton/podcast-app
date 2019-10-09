2e51d7d427c6f8d1ac5c8c6d1b59cd3c52bb06de
Kristin Skelton <kristinskelton@Kristins-MacBook-Air.local>
2019-10-09 13:11:18 -0600

    removed Dist from git

--- server/app.py
+++ server/app.py
@@ -1,9 +1,30 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template, jsonify
+todos = ['Study Vue', 'Study Flask', 'Study Toy Problems']
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+app = Flask(__name__,
+    static_folder = "./dist/static",
+    template_folder = "./dist"
+)
+
+@app.route('/')
+def serve_vue_app():
+    """
+    Serve our Vue App
+    """
+    return render_template('index.html')
+
+@app.route('/todos', methods=['GET'])
+def serve_all_todos():
+    return jsonify(items = todos)
+
+@app.after_request
+def add_header(req):
+    """
+    Clear Cache for hot-reloading so you don't have to do a hard refresh each time
+    """
+    req.headers["Cache-Control"] = "no-cache"
+    return req
+
 
 if __name__ == "__main__":
     app.run(debug=True)
